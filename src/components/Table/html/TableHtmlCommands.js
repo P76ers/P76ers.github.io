b91c09b26272b5d3aa26838efd06bd9d7f2e964e
P <117830434+P76ers@users.noreply.github.com>
2023-06-25 13:14:53 +0200

    add table html

---
+++ src/components/Table/html/TableHtmlCommands.js
@@ -0,0 +1,134 @@
+import React, { useMemo } from 'react';
+import { useTable, useSortBy, useGlobalFilter, useFilters } from 'react-table';
+import Data from './commands.json';
+import { GlobalFilter } from '../GlobalFilter';
+import { ColumnFilter } from '../ColumnFilter';
+import CodeBlock from '@theme/CodeBlock';
+
+export const TableHtmlCommands = () => {
+  const COLUMNS = [
+    {
+      Header: 'Designation',
+      accessor: 'designation',
+      Filter: ColumnFilter,
+      sortType: (prev, curr, columnId) => {
+        return sortItems(prev, curr, columnId);
+      },
+    },
+    {
+      Header: 'command',
+      accessor: 'command',
+      Cell: ({ row: { original } }) => {
+        return <CodeBlock language="html">{original.command}</CodeBlock>;
+      },
+      Filter: ColumnFilter,
+      sortType: (prev, curr, columnId) => {
+        return sortItems(prev, curr, columnId);
+      },
+    },
+    {
+      Header: 'Description',
+      accessor: 'description',
+      Filter: ColumnFilter,
+      sortType: (prev, curr, columnId) => {
+        return sortItems(prev, curr, columnId);
+      },
+    },
+  ];
+
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => Data, []);
+
+  const sortItems = (prev, curr, columnId) => {
+    if (
+      prev.original[columnId].toLowerCase() >
+      curr.original[columnId].toLowerCase()
+    ) {
+      return 1;
+    } else if (
+      prev.original[columnId].toLowerCase() <
+      curr.original[columnId].toLowerCase()
+    ) {
+      return -1;
+    } else {
+      return 0;
+    }
+  };
+
+  const sortees = React.useMemo(
+    () => [
+      {
+        id: 'designation',
+        asc: true,
+      },
+    ],
+    []
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state,
+    setGlobalFilter,
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: {
+        sortBy: sortees,
+      },
+    },
+    useFilters,
+    useGlobalFilter,
+    useSortBy
+  );
+
+  const { globalFilter } = state;
+
+  return (
+    <>
+      <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? ' 🔽'
+                        : ' 🔼'
+                      : ''}
+                  </span>
+                  <div>{column.canFilter ? column.render('Filter') : null}</div>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default TableHtmlCommands;
--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -25,6 +25,8 @@ import ColumnFilter from '../components/Table/ColumnFilter';
 import TableAutohotkeyCommands from '../components/Table/autohotkey/TableAutohotkeyCommands';
 import TableAutohotkeyScripts from '../components/Table/autohotkey/TableAutohotkeyScripts';
 
+import TableHtmlCommands from '../components/Table/html/TableHtmlCommands';
+
 import TableIntellijQuickLists from '../components/Table/intelliJ/TableIntellijQuickLists';
 import TableIntellijGeneral from '../components/Table/intelliJ/TableIntellijGeneral';
 import TableIntellijBuild from '../components/Table/intelliJ/TableIntellijBuild';
@@ -102,6 +104,8 @@ export default {
 
   TableAutohotkeyCommands,
   TableAutohotkeyScripts,
+
+  TableHtmlCommands,
 
   TableIntellijGeneral,
   TableIntellijBuild,
